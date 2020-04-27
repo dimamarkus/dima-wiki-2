f7b618b9623efe01e975489717f98139e63bc7f9
Dima Markus <dmi3000@gmail.com>
2020-04-27 16:38:42 -0400

    Update config files

--- gatsby-node.js
+++ gatsby-node.js
@@ -85,3 +85,30 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
+
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+exports.onCreateWebpackConfig = ({ stage, loaders, plugins, actions }) => {
+  if (stage === 'build-html') {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /react-ace/,
+            use: loaders.null(),
+          },
+        ],
+      },
+      plugins: [
+        plugins.define({
+          HTMLElement: {},
+          window: {},
+        }),
+      ],
+    });
+  }
+};
+

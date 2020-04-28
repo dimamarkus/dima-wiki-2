7a57f667c1c1b1255c76cc1c93e48f8a38d739d4
Dima Markus <dmi3000@gmail.com>
2020-04-28 14:50:15 -0400

    Replace index with about page content

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,159 +1,59 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
+import Content, { HTMLContent } from 'src/components/Content'
 
-import Features from 'src/components/Features'
-import BlogRoll from 'src/components/BlogRoll'
+export const IndexPageTemplate = ({ title, content, contentComponent }) => {
+  const PageContent = contentComponent || Content
 
-import {
-  EuiButton as Button,
-  EuiPage as Page,
-  EuiPageBody as Body,
-  EuiPageContent as Content,
-  EuiPageContentBody as ContentBody,
-  EuiPageContentHeader as ContentHeader,
-  EuiPageContentHeaderSection as ContentHeaderSection,
-  EuiPageHeader as Header,
-  EuiPageHeaderSection as HeaderSection,
-  EuiText as Text,
-  EuiTitle as Title,
-} from '@elastic/eui'
-
-export const IndexPageTemplate = ({
-  image,
-  title,
-  heading,
-  subheading,
-  mainpitch,
-  description,
-  intro,
-}) => (<div></div>)
-// (
-//   <Page restrictWidth>
-//     <Body>
-//       <Header>
-//         <HeaderSection>
-//           <Title size="l">
-//             <h1>{ title }</h1>
-//           </Title>
-//         </HeaderSection>
-//         <HeaderSection>
-//           <Button
-//             iconType="logoGithub"
-//             href="https://github.com/elastic/gatsby-eui-starter"
-//             fill>
-//             Open in Github
-//           </Button>
-//         </HeaderSection>
-//       </Header>
-//       <Content>
-//         <ContentHeader>
-//           <ContentHeaderSection>
-//             <Title>
-//               <h2>{ subheading }</h2>
-//             </Title>
-//           </ContentHeaderSection>
-//           <ContentHeaderSection />
-//         </ContentHeader>
-//         <ContentBody>
-//           <Title>
-//             <h3>{mainpitch.title}</h3>
-//           </Title>
-//           <Text>
-//             <p>
-//               {mainpitch.description}
-//             </p>
-//             <h3>Running locally</h3>
-//             <h3>
-//               {heading}
-//             </h3>
-//             <p>{description}</p>
-//             <Link className="btn" to="/products">
-//               See all products
-//             </Link>
-//             <h3>
-//               Latest stories
-//             </h3>
-//           </Text>
-//           <Features gridItems={intro.blurbs} />
-//           <BlogRoll />
-//         </ContentBody>
-//       </Content>
-//     </Body>
-//   </Page>
-// )
+  return (
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="section">
+              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                {title}
+              </h2>
+              <PageContent className="content" content={content} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
 
 IndexPageTemplate.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  title: PropTypes.string,
-  heading: PropTypes.string,
-  subheading: PropTypes.string,
-  mainpitch: PropTypes.object,
-  description: PropTypes.string,
-  intro: PropTypes.shape({
-    blurbs: PropTypes.array,
-  }),
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string,
+  contentComponent: PropTypes.func,
 }
 
 const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
+  const { markdownRemark: post } = data
 
   return (
     <IndexPageTemplate
-      image={frontmatter.image}
-      title={frontmatter.title}
-      heading={frontmatter.heading}
-      subheading={frontmatter.subheading}
-      mainpitch={frontmatter.mainpitch}
-      description={frontmatter.description}
-      intro={frontmatter.intro}
+      contentComponent={HTMLContent}
+      title={post.frontmatter.title}
+      content={post.html}
     />
   )
 }
 
 IndexPage.propTypes = {
-  data: PropTypes.shape({
-    markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object,
-    }),
-  }),
+  data: PropTypes.object.isRequired,
 }
 
 export default IndexPage
 
-export const pageQuery = graphql`
-  query IndexPageTemplate {
-    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+export const indexPageQuery = graphql`
+  query IndexPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
       frontmatter {
         title
-        image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        heading
-        subheading
-        mainpitch {
-          title
-          description
-        }
-        description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-            text
-          }
-          heading
-          description
-        }
       }
     }
   }
